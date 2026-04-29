70fa24da2f57dd41c7119eff0e809839f6aba0de
Princess-Pea <jerrypeterli@outlook.com>
2026-04-29 23:43:57 +0800

    feat: learn iterative siftup

--- DataStructures_Cpp/heap.cpp
+++ DataStructures_Cpp/heap.cpp
@@ -27,7 +27,7 @@ int heap[N] = {0}; // 堆数组
 int size = 0;      // 堆的大小
 
 // 堆化，此处为大顶堆
-void siftup(int i) // i 是需要堆化的节点索引
+void recursive_siftup(int i) // i 是需要堆化的节点索引
 {
     if (i == 0)
     {
@@ -36,6 +36,22 @@ void siftup(int i) // i 是需要堆化的节点索引
     if (heap[i] > heap[parent(i)])
     {
         swap(heap[i], heap[parent(i)]);
-        siftup(parent(i)); // 写在if内，只在需要交换时才继续向上调整
+        recursive_siftup(parent(i)); // 写在if内，只在需要交换时才继续向上调整
+    }
+}
+
+void iterative_siftup(int i)
+{
+    while (i > 0)
+    {
+        if (heap[i] > heap[parent(i)])
+        {
+            swap(heap[i], heap[parent(i)]);
+            i = parent(i); // 交换后继续向上调整
+        }
+        else
+        {
+            break; // 如果不需要交换，直接退出循环
+        }
     }
 }
